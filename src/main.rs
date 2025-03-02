db565bd35e5f65fd36ea5334d08102f6c737f6a7
Jeromos Kovács <iitsnotme214@proton.me>
2025-03-02 16:28:20 +0100

    refactor: pedantic clippy fixes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use clap::Parser;
 use indexmap::IndexMap;
-use std::path::Path;
-use std::path::PathBuf;
 use std::{
-    fs,
-    io::{self, Read, Write},
+    fs::{self, File},
+    io::{self, copy, Read, Write},
+    path::{Path, PathBuf},
     process::{Command, Stdio},
-};
-use std::{
-    fs::File,
-    io::copy,
     time::{SystemTime, UNIX_EPOCH},
 };
 use tokio::task;
-use toml::Value;
-use wayland_clipboard_listener::WlClipboardPasteStream;
-use wayland_clipboard_listener::WlListenType;
+use toml::{value::Table, Value};
+use wayland_clipboard_listener::{WlClipboardPasteStream, WlListenType};
 use wl_clipboard_rs::copy::{MimeSource, MimeType, Options, Source};
 use wl_clipboard_rs::paste::{get_contents, ClipboardType, Seat};
 use xdg::BaseDirectories;
@@ -38,177 +32,173 @@ async fn main() {
         .place_config_file("config.toml")
         .expect("cannot create configuration directory");
 
-    let toml_string = fs::read_to_string(config_path).unwrap_or(String::from(""));
+    let toml_string = fs::read_to_string(config_path).unwrap_or_default();
     let value: Value = toml::from_str(&toml_string).unwrap();
 
-    let default_launcher = vec!["tofi", "--fuzzy-match=true", "--prompt-text=clapboard: "];
+    let default_launcher = ["tofi", "--fuzzy-match=true", "--prompt-text=clapboard: "];
 
     let default_launcher_values: Vec<Value> = default_launcher
         .iter()
-        .map(|x| Value::String(x.to_string()))
+        .map(|x| Value::String((*x).to_string()))
         .collect();
     let default_launcher_value = Value::Array(default_launcher_values);
     let launcher = value
         .get("launcher")
-        .unwrap_or_else(|| &default_launcher_value)
+        .unwrap_or(&default_launcher_value)
         .as_array();
 
     let history_size = value
         .get("history_size")
-        .and_then(|v| v.as_integer())
+        .and_then(Value::as_integer)
         .unwrap_or(50) as usize;
 
-    let default_favorites_value = Value::Table(toml::value::Table::new());
+    let default_favorites_value = Value::Table(Table::new());
     let favorites = value
         .get("favorites")
-        .unwrap_or_else(|| &default_favorites_value)
+        .unwrap_or(&default_favorites_value)
         .as_table()
         .unwrap();
 
     let cache_dir = xdg_dirs.get_cache_home();
 
-    match args.record {
-        Some(record) => {
-            println!("Clapboard recording {record}...");
-            let listeners = if record == "primary" {
-                vec!["primary"]
-            } else if record == "clipboard" {
-                vec!["clipboard"]
-            } else if record == "both" {
-                vec!["primary", "clipboard"]
-            } else {
-                vec![]
-            };
+    if let Some(record) = args.record {
+        println!("Clapboard recording {record}...");
+        let listeners = if record == "primary" {
+            vec!["primary"]
+        } else if record == "clipboard" {
+            vec!["clipboard"]
+        } else if record == "both" {
+            vec!["primary", "clipboard"]
+        } else {
+            vec![]
+        };
 
-            // Spawn tasks for each listener
-            let tasks: Vec<_> = listeners
-                .iter()
-                .map(|&paste_type| {
-                    task::spawn(listen_to_clipboard(
-                        paste_type,
-                        cache_dir.clone(),
-                        history_size,
-                    ))
-                })
-                .collect();
+        // Spawn tasks for each listener
+        let tasks: Vec<_> = listeners
+            .iter()
+            .map(|&paste_type| {
+                task::spawn(listen_to_clipboard(
+                    paste_type,
+                    cache_dir.clone(),
+                    history_size,
+                ))
+            })
+            .collect();
 
-            // Await each task individually
-            for task in tasks {
-                let _ = task.await;
-            }
+        // Await each task individually
+        for task in tasks {
+            let _ = task.await;
         }
-        None => {
-            let mut data: IndexMap<String, String> = IndexMap::new();
+    } else {
+        let mut data: IndexMap<String, String> = IndexMap::new();
 
-            let mut entries: Vec<_> = fs::read_dir(&cache_dir)
-                .unwrap() // Handle the Result from read_dir
-                .flatten() // Flatten the Result<Option<DirEntry>> to just DirEntry
-                .collect(); // Collect into a vector of DirEntry
+        let mut entries: Vec<_> = fs::read_dir(&cache_dir)
+            .unwrap() // Handle the Result from read_dir
+            .flatten() // Flatten the Result<Option<DirEntry>> to just DirEntry
+            .collect(); // Collect into a vector of DirEntry
 
-            // Sort entries by file name (ascending order)
-            entries.sort_by(|a, b| b.file_name().cmp(&a.file_name()));
+        // Sort entries by file name (ascending order)
+        entries.sort_by(|a, b| b.file_name().cmp(&a.file_name()));
 
-            // Iterate over sorted entries
-            for entry in entries {
-                if entry.path().is_dir() {
-                    let timestamp = entry.file_name().into_string().unwrap_or_default();
-                    let text_files =
-                        vec!["UTF8_STRING", "TEXT", "text.plain", "text.html", "STRING"];
-                    let mut found_file = false;
-                    let mut content = String::new();
-                    for file_name in text_files {
-                        let textual_representation = entry.path().join(file_name);
+        // Iterate over sorted entries
+        for entry in entries {
+            if entry.path().is_dir() {
+                let timestamp = entry.file_name().into_string().unwrap_or_default();
+                let text_files = vec!["UTF8_STRING", "TEXT", "text.plain", "text.html", "STRING"];
+                let mut found_file = false;
+                let mut content = String::new();
+                for file_name in text_files {
+                    let textual_representation = entry.path().join(file_name);
 
-                        if textual_representation.exists() {
-                            let mut file = File::open(&textual_representation).unwrap();
-                            if file.read_to_string(&mut content).is_ok() {
-                                found_file = true;
-                                break;
-                            }
+                    if textual_representation.exists() {
+                        let mut file = File::open(&textual_representation).unwrap();
+                        if file.read_to_string(&mut content).is_ok() {
+                            found_file = true;
+                            break;
                         }
                     }
-                    if found_file {
-                        data.insert(
-                            content
-                                .trim()
-                                .to_string()
-                                .replace("\n", " ")
-                                .chars()
-                                .take(50) // Avoid long text
-                                .collect(),
-                            timestamp.to_string(),
-                        );
-                    } else {
-                        // If no file was found, proceed with the else logic
-                        println!("No textfile found for: {}", timestamp.to_string());
-                        data.entry(timestamp.to_string())
-                            .or_insert_with(|| timestamp.to_string());
-                    }
+                }
+                if found_file {
+                    data.insert(
+                        content
+                            .trim()
+                            .to_string()
+                            .replace('\n', " ")
+                            .chars()
+                            .take(50) // Avoid long text
+                            .collect(),
+                        timestamp.to_string(),
+                    );
+                } else {
+                    // If no file was found, proceed with the else logic
+                    println!("No textfile found for: {timestamp}");
+                    data.entry(timestamp.to_string())
+                        .or_insert_with(|| timestamp.to_string());
                 }
             }
-            for (key, value) in favorites {
-                data.entry(key.parse().unwrap())
-                    .or_insert_with(|| value.as_str().unwrap().to_string());
-            }
+        }
+        for (key, value) in favorites {
+            data.entry(key.parse().unwrap())
+                .or_insert_with(|| value.as_str().unwrap().to_string());
+        }
 
-            let input = data.keys().cloned().collect::<Vec<_>>().join("\n");
-            let command_name = launcher.unwrap()[0].as_str().unwrap();
-            let mut command = Command::new(command_name);
-            for arg in &launcher.unwrap()[1..] {
-                command.arg(arg.as_str().unwrap());
-            }
+        let input = data.keys().cloned().collect::<Vec<_>>().join("\n");
+        let command_name = launcher.unwrap()[0].as_str().unwrap();
+        let mut command = Command::new(command_name);
+        for arg in &launcher.unwrap()[1..] {
+            command.arg(arg.as_str().unwrap());
+        }
 
-            let output = command
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
-                .and_then(|mut child| {
-                    child.stdin.as_mut().unwrap().write_all(input.as_bytes())?;
-                    child.wait_with_output()
-                })
-            .unwrap_or_else(|_| panic!("Cannot start your launcher, please confirm you have {} installed or configure another one", command_name));
+        let output = command
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .and_then(|mut child| {
+                child.stdin.as_mut().unwrap().write_all(input.as_bytes())?;
+                child.wait_with_output()
+            })
+        .unwrap_or_else(|_| panic!("Cannot start your launcher, please confirm you have {command_name} installed or configure another one"));
 
-            let mut result = String::from_utf8_lossy(&output.stdout).into_owned();
-            result.pop(); // Remove trailing new line
-            if result.len() > 0 {
-                let mut opts = Options::new();
-                opts.foreground(true); // We need to keep the process alive for pasting to work
-                if favorites.contains_key(&result) {
-                    opts.copy(
-                        Source::Bytes(
-                            data.get(&result)
-                                .unwrap()
-                                .to_string()
-                                .into_bytes()
-                                .into_boxed_slice(),
-                        ),
-                        MimeType::Autodetect,
-                    )
-                    .expect("Failed to copy to clipboard");
-                } else {
-                    let prefix = data.get(&result).unwrap().as_str();
-                    let sources: Vec<MimeSource> =
-                        fs::read_dir(format!("{}{}", cache_dir.to_str().unwrap(), prefix))
+        let mut result = String::from_utf8_lossy(&output.stdout).into_owned();
+        result.pop(); // Remove trailing new line
+        if !result.is_empty() {
+            let mut opts = Options::new();
+            opts.foreground(true); // We need to keep the process alive for pasting to work
+            if favorites.contains_key(&result) {
+                opts.copy(
+                    Source::Bytes(
+                        data.get(&result)
                             .unwrap()
-                            .flatten()
-                            .filter_map(|entry| {
-                                let path = entry.path();
-                                let mime_type = path
-                                    .file_name()?
-                                    .to_string_lossy()
-                                    .to_string()
-                                    .replacen(".", "/", 1);
-                                fs::read(&path).ok().map(|contents| MimeSource {
-                                    source: Source::Bytes(contents.into()),
-                                    mime_type: MimeType::Specific(mime_type),
-                                })
+                            .to_string()
+                            .into_bytes()
+                            .into_boxed_slice(),
+                    ),
+                    MimeType::Autodetect,
+                )
+                .expect("Failed to copy to clipboard");
+            } else {
+                let prefix = data.get(&result).unwrap().as_str();
+                let sources: Vec<MimeSource> =
+                    fs::read_dir(format!("{}{}", cache_dir.to_str().unwrap(), prefix))
+                        .unwrap()
+                        .flatten()
+                        .filter_map(|entry| {
+                            let path = entry.path();
+                            let mime_type = path
+                                .file_name()?
+                                .to_string_lossy()
+                                .to_string()
+                                .replacen('.', "/", 1);
+                            fs::read(&path).ok().map(|contents| MimeSource {
+                                source: Source::Bytes(contents.into()),
+                                mime_type: MimeType::Specific(mime_type),
                             })
-                            .collect();
+                        })
+                        .collect();
 
-                    if !sources.is_empty() {
-                        opts.copy_multi(sources)
-                            .expect("Failed to copy to clipboard");
-                    }
+                if !sources.is_empty() {
+                    opts.copy_multi(sources)
+                        .expect("Failed to copy to clipboard");
                 }
             }
         }
@@ -239,19 +229,19 @@ async fn listen_to_clipboard(paste_type: &str, cache_dir: PathBuf, history_size:
                 Ok((mut reader, _)) => {
                     let path = format!("{}{}", cache_dir.to_str().unwrap(), timestamp);
                     fs::create_dir_all(Path::new(&path)).unwrap();
-                    let file_path = format!("{}/{}", &path, mime.replace("/", "."));
+                    let file_path = format!("{}/{}", &path, mime.replace('/', "."));
                     match File::create(&file_path) {
                         Ok(mut file) => {
                             if let Err(e) = copy(&mut reader, &mut file) {
-                                eprintln!("Failed to copy content to {}: {}", file_path, e);
+                                eprintln!("Failed to copy content to {file_path}: {e}");
                             }
                         }
                         Err(e) => {
-                            eprintln!("Failed to create file {}: {}", file_path, e);
+                            eprintln!("Failed to create file {file_path}: {e}");
                         }
                     }
                 }
-                Err(err) => eprintln!("Clipboard {paste_type:?} error: {}", err),
+                Err(err) => eprintln!("Clipboard {paste_type:?} error: {err}"),
             }
         }
         clean_history(&cache_dir, history_size).unwrap();
@@ -260,7 +250,7 @@ async fn listen_to_clipboard(paste_type: &str, cache_dir: PathBuf, history_size:
 
 fn clean_history(directory: &Path, max: usize) -> io::Result<()> {
     let mut entries: Vec<_> = fs::read_dir(directory)?
-        .filter_map(|entry| entry.ok()) // Ignore any errors in reading entries
+        .filter_map(Result::ok) // Ignore any errors in reading entries
         .collect();
 
     entries.sort_by(|a, b| b.file_name().cmp(&a.file_name()));
